9fce2eed1be8a30208a8869e12532152766e1a98
edanielbluford <e.daniel.bluford@gmail.com>
2018-07-10 11:50:32 -0400

    first commit

---
+++ MVCPraticeTwo/Models/VirtualPet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCPraticeTwo.Models
+{
+    public class VirtualPet
+    {
+        [Key]
+        public int VirtualPetID { get; set; }
+        [DisplayName("Name of Pet")]
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Hunger { get; set; }
+        public int Thirst { get; set; }
+    }
+}
